07d457d11debb76f2ea8de5931166e2e7096e84f
RodrigoCamposD <campos6@gmail.com>
2023-03-25 20:29:28 -0300

    overview router and controller

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const globalErrorHandler = require("./controllers/errorController");
 const tourRoutes = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
+const viewRouter = require("./routes/viewRoutes");
 
 const limiter = rateLimit({
   max: 100,
@@ -50,26 +51,10 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use("/", viewRouter);
 app.use("/api/v1/tours", tourRoutes);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);
-
-app.get("/", (req, res) => {
-  res.status(200).render("base", {
-    tour: "The Forest Hiker",
-    user: "Jonas",
-  });
-});
-app.get("/overview", (req, res) => {
-  res.status(200).render("overview", {
-    title: "All tours",
-  });
-});
-app.get("/tour", (req, res) => {
-  res.status(200).render("tour", {
-    title: "The Forest Hiker",
-  });
-});
 
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
